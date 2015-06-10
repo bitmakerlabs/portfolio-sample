295123ebc73f2078f94dcab71c80032918e45b1c
juneate <roccopan@gmail.com>
2015-06-10 19:53:09 -0400

    creating our own scrolling effects

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A simple example of a portfolio.
 
 ## To Do:
 
-- When the page scrolls, slide the logo up at half the logo up at half the rate
+- When the page scrolls, slide the logo up at half the rate of the page
 - When the page has fully scrolled, hide the logo, show the top bar
 - When the screen goes over 768px, note the tiling issue with the image - solve that with the 'masonry' plugin
   - Why is this happened anyways?!
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,27 @@
+$(document).on('scroll', function() {
 
+	// Num of px scrolled
+	var pxscrolled = $(this).scrollTop();
+
+	// Get the window height
+	var winh = $(window).height();
+
+	// Get the percentage of the page that's been scrolled
+	var percentscrolled = pxscrolled / winh;
+
+	// For every px scrolled, move the logo 0.5px up
+	//    ** It goes half px because top and bottom are pulling
+	// Also set the inverse percentage of scroll as the opacity
+	$('.logo')
+		.css('top', -pxscrolled)
+		.css('opacity', 1 - percentscrolled);
+
+	// If the browser has scrolled to the top one full window...
+	if (pxscrolled > (winh - 70)) {
+		$('.bar').fadeIn();
+	}
+	else {
+		$('.bar').fadeOut();
+	}
+
+});
