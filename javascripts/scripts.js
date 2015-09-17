aa88810256f94e2802e54b0d9c7c85cf7a302f68
Rocco Panacci <rocco@clausehound.com>
2015-09-16 20:36:09 -0400

    Did some drills to improve the application

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,40 @@
+	// How many px have I scrolled?
+$(window).on('scroll', function() {
+
+		// Height of the window
+	var w = $(window).height();
+
+		// Number of px scrolled
+	var px = $(window).scrollTop();
+	
+
+	// 1. Move the logo in relation to the scroller (1/2)
+	
+		// This is how I move an element up
+		// Note: -100, actually moves -50
+	$('.logo').css('top', -px);
+
+
+	// 2. *IF* the amount scrolled (px) exceeds (>) the window height (w), .fadeIn() the .bar
+	console.log("px scrolled:", px, "w height:", w);
+
+	if (px > w) {
+		$('.bar').fadeIn();
+	}
+	else {
+		$('.bar').fadeOut();
+	}
+
+	// 3. Use the % scrolled to affect transparency
+	var percent = px / w;
+	$('.logo').css('opacity', 1-percent);
+
+
+});
+
+
+
+
+
+
 
